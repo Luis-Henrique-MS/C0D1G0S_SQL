25ce01258affe793348171ec08a093d3c4af0633
Luis Henrique Maicá de Souza <lhms2409@gmail.com>
2025-11-16 23:09:40 -0300

    Add files via upload

---
+++ aula_04.sql
@@ -0,0 +1,13 @@
+
+select * from escola;
+
+select * from escola
+where nome = 'Joao silva';
+
+select * from escola
+order by nome desc;
+
+select coluna1, coluna2 from escola;
+
+select * from escola
+where ano <= 2016;
